be1053dbe1d0427651fddbf7ef361d3bb969a8cd
Egbert HU <62612263+gottaegbert@users.noreply.github.com>
2022-06-30 16:05:45 +0200

    update

---
+++ Assets/emojpiechart.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class emojpiechart : MonoBehaviour
+{
+    public MainScript script;
+    double[] newResults;
+    public GameObject PieChart;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //mainScript = GameObject.Find("Script").getComponent();
+       // Mainscript
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newResults = script.results;
+        Debug.Log(PieChart);
+        //Debug.Log("anger:" + newResults[3]);
+    }
+}
