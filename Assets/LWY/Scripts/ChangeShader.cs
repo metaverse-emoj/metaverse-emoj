2af3b5ed496e27dc81220637d3cb6c472481bb9b
Egbert HU <62612263+gottaegbert@users.noreply.github.com>
2022-06-30 16:49:02 +0200

    tryshadercolorchange

--- Assets/LWY/Scripts/ChangeShader.cs
+++ Assets/LWY/Scripts/ChangeShader.cs
@@ -4,30 +4,32 @@ using UnityEngine;
 
 public class ChangeShader : MonoBehaviour
 {
-    private Material material;
+    public GameObject Ch03;
 
     
     void Start()
     {
-        material= GetComponent<Renderer>().material;
+       
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)){
+        if (Input.GetKeyDown(KeyCode.M)){
+            Debug.Log("@##$#$");
 
+            Ch03.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
             //Debug.Log(material.name);
 
-            material.SetColor("_Color",Color.blue);
+            //material.SetColor("_Color",Color.blue);
 
             //material.color=Color.blue;
             //material.SetVector("_EdgeColor",new Vector4(1,1,1,1));
             //material.SetFloat("_Dissolve",0.9f);
 
-            GetComponent<Renderer>().material = material;
-            
+            //GetComponent<Renderer>().material = material;
+
         }
        
     }
